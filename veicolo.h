9b77b62f3d05743528c3ea3130d20af2f217233a
Pruizcoder <74655983+Pruizcoder@users.noreply.github.com>
2022-05-23 09:32:53 +0200

    created a header files

---
+++ veicolo.h
@@ -0,0 +1,16 @@
+#ifndef VEICOLO_H
+#define VEICOLO_H
+#include <iostream>
+using namespace std;
+class veicolo
+{
+private:
+	string marca;
+	string modello;
+	int cilindrata;
+	int cavalli;
+public:
+	string getMarca();
+
+};
+#endif // VEICOLO_H
